a1cdb6bc60a7d9191a25d764108a07c51172c96b
ethanmac <ethanmac@mac.com>
2021-04-17 11:44:30 +0800

    update

--- muzero/networks/cartpole_network.py
+++ muzero/networks/cartpole_network.py
@@ -1,9 +1,9 @@
 import math
 
 import numpy as np
-from tensorflow_core.python.keras import regularizers
-from tensorflow_core.python.keras.layers.core import Dense
-from tensorflow_core.python.keras.models import Sequential
+from tensorflow.python.keras import regularizers
+from tensorflow.python.keras.layers.core import Dense
+from tensorflow.python.keras.models import Sequential
 
 from game.game import Action
 from networks.network import BaseNetwork
